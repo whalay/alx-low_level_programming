6e9c72589f0e7f360b55c323beabf127742bf9be
Yusuf <soliuy5@gmail.com>
2022-10-13 06:56:16 -0700

    added

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
  * main- EntryPoint
  *
- * return: alway 
+ * Return: Always 0 (Success) 
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");

874bf0bbc40b6838c27a9733619d3b769174415a
Yusuf <soliuy5@gmail.com>
2022-10-16 02:59:24 -0700

    added

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry - point
+ *
+ * Return: Always success (0)
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = i + 1; j < 8; j++)
+		{
+			putchar(i + '0')
+			if (i < 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+
+	}
+	putchar('\n');
+
+return (0);
+}
+

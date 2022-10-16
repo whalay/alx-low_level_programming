a2d813c40ece7b06b08676b0114335c705aa6f2b
Yusuf <soliuy5@gmail.com>
2022-10-16 02:24:32 -0700

    addeds

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char hex;
+
+	for (hex = '0'; hex <= '9'; hex++)
+	{
+		putchar(hex);
+	}
+	for (hex = 'a'; hex <= 'f'; hex++)
+	{
+		putchar(hex);
+	}
+
+	putchar('\n');
+
+return (0);
+}
+

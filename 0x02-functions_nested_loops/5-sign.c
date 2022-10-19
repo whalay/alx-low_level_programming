f0a410fad87e1446c8a4617ac07129122404b9c4
Yusuf <soliuy5@gmail.com>
2022-10-18 22:39:37 -0700

    added

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include <main.h>
+/**
+ * print_sign - Print a number iif it i
+ * negative or positive
+ *
+ * @n: return 1 or 0
+ * Return: returns 0 or 1
+ */
+
+int print_sign(int n)
+{
+	if (n == 0)
+	{
+		return (0);
+	}
+	else if (n > 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (-1);
+	}
+
+	_putchar('\n');
+
+}
+

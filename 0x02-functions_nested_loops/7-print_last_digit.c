f68db95df1decb4deff6c00d0ad5136d176280f1
Yusuf <soliuy5@gmail.com>
2022-10-19 13:29:10 -0700

    aded

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_last_digit - check fr last number
+ * @n: print 0 or -1
+ I*
+ * Return: success aways
+ */
+
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n % 10;
+
+	if (l < 0)
+	{
+		_putchar(-l + 48);
+		return (-1);
+	}
+	else
+	{
+		_putchar(l + 48);
+		return (1);
+	}
+
+}

3a343c8ae688ad67306d307c8fdf2cd3d6db9600
Yusuf <soliuy5@gmail.com>
2022-10-18 22:52:21 -0700

    added

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,15 +11,19 @@ int print_sign(int n)
 {
 	if (n == 0)
 	{
+		_putchar(48);
 		return (0);
 	}
 	else if (n > 0)
 	{
-		return ('+' + 1);
+		_putchar(43);
+		return (1);
 	}
 	else
 	{
-		return ('-' + 1);
+
+		_putchar(45);
+		return (1);
 	}
 
 	_putchar('\n');

3f7c4127ffcc351449c01ff9b21394b73f696478
Yusuf <soliuy5@gmail.com>
2022-10-19 14:18:13 -0700

    added git

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * 24_hours - check fr last number
+ * 
+ * Return: success aways
+ */
+
+void jack_bauer(void)
+{
+	for (a = 48; a <= 50; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			for (c = 48; c <= 53; c++)
+			{
+				for (d = 48; d <= 52; d++)
+				{
+					if (a >= 50 && b >= 52)
+					{
+						_putchar(a);
+						_putchar(b);
+						_putchar(58);
+						_putchar(c);
+						_putchar(d);
+						_putchar('\n');
+					}
+				}
+			}
+		}
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	

15191abb6e189b7292d05ce0932ff6e08f3c5bed
Yusuf <soliuy5@gmail.com>
2022-10-19 22:02:42 -0700

    added

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "stdio.h"
+
+/**
+ * print_to_98 - a function that adds two integers
+ *
+ * and returns the result.
+ * @n: number one.
+ *
+ * Return: Add of void print_tumber two.
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for ; n > 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("%d\n", n);
+}

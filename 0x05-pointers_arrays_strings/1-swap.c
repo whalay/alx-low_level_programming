c91457245d85cc45e1d2076a86840b3e8fd4012c
Yusuf <soliuy5@gmail.com>
2022-10-24 05:03:32 -0700

    added

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swap two variable
+ *
+ * @a: the first value
+ * @b: second value
+ *
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+	
+	temp = *a;
+	*a = *b
+	*b = temp;	
+}

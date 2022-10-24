8156c8baf3c20daefbee380876754c520ba6204c
Yusuf <soliuy5@gmail.com>
2022-10-24 07:21:31 -0700

    added

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_rev - a reverse function.
+ * @s: value taken from main
+ * Return: (0)
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	for (len = 0; s[len] != '\0'; len++)
+	{
+	}
+	for (len = len - 1; len >= 0; len--)
+	{
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}

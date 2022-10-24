e887c637d7d433fe4ccf428783fdf970ba8fa4c5
Yusuf <soliuy5@gmail.com>
2022-10-24 06:25:51 -0700

    added

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - print a striing follw by new line
+ *
+ * @str: an inputted string
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	putchar(str);
+	putchar('\n');
+}

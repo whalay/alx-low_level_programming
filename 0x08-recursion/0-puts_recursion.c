e0c4485f809a10de29303dd9a6eead968cf47598
Yusuf <soliuy5@gmail.com>
2022-11-03 09:07:41 -0700

    added

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - main. priints a char and a new line
+ *
+ * @s: The char to be outputed
+ *
+ * Return: returns nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}

8dfb505d55610bdc7721de8205d0741b4dceaa62
Yusuf <soliuy5@gmail.com>
2022-11-05 03:56:20 -0700

    added

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include  <stdio.h>
+
+/**
+ * main - prints the  total number of args
+ *
+ * @argc: arguement1
+ * @argv: arguement2
+ *
+ * Return: 0
+ */
+
+int main (int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}

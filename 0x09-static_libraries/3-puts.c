8380464f3603c5848f2039f691d9c1d4fec19553
Yusuf <soliuy5@gmail.com>
2022-11-07 02:52:41 -0800

    adedd

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -11,12 +11,13 @@
 
 void _puts(char *str)
 {
-	int i = 0;
+        int i = 0;
 
-	while (*(str + 1) != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
-	_putchar('\n');
+        while (*(str + 1) != '\0')
+        {
+                _putchar(str[i]);
+                i++;
+        }
+        _putchar('\n');
 }
+

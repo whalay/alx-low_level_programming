1193b7329cf99cec58324483cf50e93757415d44
Yusuf <soliuy5@gmail.com>
2022-11-07 05:56:56 -0800

    added

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if  it is number
+ *
+ * @c: Ascii character
+ *
+ * Return: 1 if it is a number. 0  otherwise
+ */
+
+int _isalpha(int c)
+{
+
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
+
